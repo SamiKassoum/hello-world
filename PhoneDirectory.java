3550c2782338001b1539897555449732e852bdce
SamiKassoum <skassoum@gmail.com>
2016-07-08 13:39:33 +0300

    Create PhoneDirectory.java

---
+++ PhoneDirectory.java
@@ -0,0 +1,10 @@
+/*
+this programe is intended to make a simpel phone directory,
+i am doing small excersises on TreeMap
+/*
+
+public PhoneDirectory{
+  public static void main (String [] Args){
+    System.out.println("Test if input goes anywhere");
+  }
+}
